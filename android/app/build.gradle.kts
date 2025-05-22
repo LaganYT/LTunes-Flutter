d73637f08f1f35607dcff7f9c0b69a8b8f79706c
Logan <95306463+LaganYT@users.noreply.github.com>
2025-05-21 23:42:39 -0500

    fix: update ndkVersion in build.gradle.kts to a specific version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.LTunes"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion // Changed from hardcoded to flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // Changed from flutter.ndkVersion to specific version
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
